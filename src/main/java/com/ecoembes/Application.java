9fb71243a8c3128e749c5c40771b7fd715338f15
AlvaroLopezdeOcariz <a.lopezdeocarizde@opendeusto.es>
2025-11-16 12:59:13 +0100

    Initial commit

---
+++ src/main/java/com/ecoembes/Application.java
@@ -0,0 +1,36 @@
+package com.ecoembes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
+
+
+/**
+
+ * Prueba en el navegador:
+
+	* http://localhost:8080/ping → debe responder ecoembesDS07 OK.
+
+	* http://localhost:8080/swagger-ui.html → debe cargar Swagger UI.
+
+*/
+
+
+/**
+
+ * Añadir el API REST al proyecto y probar en el navegador:
+
+	* com.ecoembes.p1.controller
+
+	* com.ecoembes.p1.service
+
+	* com.ecoembes.p1.dto
+
+	* com.ecoembes.p1.exception
+*/
