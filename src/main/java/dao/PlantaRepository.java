006135acbeebb7ecc71b05c59b272b013ec200fa
AnderUrien <ander.urien@opendeusto.es>
2025-12-09 18:42:16 +0100

    BD cambio

--- src/main/java/dao/PlantaRepository.java
+++ src/main/java/dao/PlantaRepository.java
@@ -2,9 +2,10 @@ package dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.entity.PlantaReciclaje;
 
-
+@Repository
 public interface PlantaRepository extends JpaRepository<PlantaReciclaje, String> {}
 
